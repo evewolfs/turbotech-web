c9e7bdb1243e3973064ea8ffd34e21a4df92c309
evewolfs <53898640+evewolfs@users.noreply.github.com>
2022-12-04 12:31:42 +0100

    deploy 7

--- src/components/pagecmp/PageHeader.js
+++ src/components/pagecmp/PageHeader.js
@@ -19,7 +19,7 @@ function PageHeader({ title, children, ...rootProps }) {
       >
         {title ? (
           <Box fontSize="clamp(1.6rem, 3vw, 3rem)" style={{lineHeight:"1.2",
-          padding: '0% 2%', margin: '0 2%', }}>
+          padding: '0% 2%', margin: '0 2%' }}>
             {title}
           </Box>
         ) : null}
